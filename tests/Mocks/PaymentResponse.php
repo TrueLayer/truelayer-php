2b40c6bfe2c2a6823aa2ec779ff04202f8c4de6d
Stefan Adrian Danaita <me@dsa.io>
2022-02-02 20:54:10 +0000

    [PHP-15] Replace paymentToken with resourceToken

--- tests/Mocks/PaymentResponse.php
+++ tests/Mocks/PaymentResponse.php
@@ -10,7 +10,7 @@ class PaymentResponse
 {
     public const CREATED = [
         'id' => '5a2a0a0d-d3ad-4740-860b-45a01bcc17ac',
-        'payment_token' => 'the-token',
+        'resource_token' => 'the-token',
         'user' => [
             'id' => 'ba6412a3-8b2c-4d33-a40b-9964062979da',
         ],
