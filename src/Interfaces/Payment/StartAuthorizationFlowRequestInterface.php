063c9a2c6418f916fea088d05d5f09e8bc7c2a41
Artyom Jaksov <artyom.jaksov@truelayer.com>
2024-09-25 13:16:06 +0200

    add notes to auth flow

--- src/Interfaces/Payment/StartAuthorizationFlowRequestInterface.php
+++ src/Interfaces/Payment/StartAuthorizationFlowRequestInterface.php
@@ -26,21 +26,32 @@ interface StartAuthorizationFlowRequestInterface extends HasAttributesInterface
 
     /**
      * @return StartAuthorizationFlowRequestInterface
+     * If you are building a custom UI, you need to specify whether your UI can render a provider selection screen.
+     * This is mandatory for user selected provider selection. For preselected, it's optional.
+     * {@see \TrueLayer\Interfaces\Provider\UserSelectedProviderSelectionInterface}
+     * Use the {@see \TrueLayer\Interfaces\Client\ClientInterface::providerFilter()} parameter
+     * at payment creation to determine the list of providers you can choose to display in your UI.
      */
     public function enableProviderSelection(): StartAuthorizationFlowRequestInterface;
 
     /**
      * @return StartAuthorizationFlowRequestInterface
+     * If you are building a custom UI, you need to specify whether your UI can render a provider selection screen.
+     * This is mandatory for user selected scheme selection. For preselected, it's optional.
+     * {@see \TrueLayer\Interfaces\Scheme\UserSelectedSchemeSelectionInterface}
      */
     public function enableSchemeSelection(): StartAuthorizationFlowRequestInterface;
 
     /**
      * @return StartAuthorizationFlowRequestInterface
+     * If you are building a custom UI, you need to specify whether your UI can render a user account selection screen.
+     * If the user has previously consented to saving their bank account details with TrueLayer,
+     * they can choose from their saved accounts to speed up following payments.
      */
     public function enableUserAccountSelection(): StartAuthorizationFlowRequestInterface;
 
     /**
-     * @param string $returnUri
+     * @param string $returnUri The URL where the user should be redirected back once the flow on the third-party's website is completed.
      *
      * @return StartAuthorizationFlowRequestInterface
      */
